59eebfc43272d55b7287d7ef95cb570412811c39
Gelilamihirke <mihirkelala@gmail.com>
2023-09-10 03:21:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is the main function of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Siae of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Siae of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Siae of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Siae of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Siae of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
