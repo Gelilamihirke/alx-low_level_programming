788694f01be19e85565d06884dafd37970853b4c
Gelilamihirke <mihirkelala@gmail.com>
2023-09-13 12:52:42 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char g;
+
+	for (g = 'a'; g <= 'z'; g++)
+	{
+		_putchar(g);
+	}
+	_putchar('\n');
+}
