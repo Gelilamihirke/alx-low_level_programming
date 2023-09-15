a2714a79448ceadd2c402b80123f483211b48f56
Gelilamihirke <mihirkelala@gmail.com>
2023-09-15 13:00:02 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_number - print the numbers from 0 to 9
+ * Return: The num from 0 to 9
+ */
+void print_number(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
