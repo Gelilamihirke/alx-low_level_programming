9576c42475cab2892945e78931442a1fa7f06290
Gelilamihirke <mihirkelala@gmail.com>
2023-09-29 10:59:14 -0700

    try0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
  */
 void _puts_recursion(char *x)
 {
+	if (*x != '\0')
+	{
+		_putchar(*x);
+		_put_recrsion(x + 1);
+	}
 	if (*x == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*x);
-	x++;
-	_put_recrsion(x);
 }
