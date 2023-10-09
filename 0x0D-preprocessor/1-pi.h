b623ef14c85c725c2c601bcd8e40bc1285e61ade
Gelilamihirke <mihirkelala@gmail.com>
2023-10-09 16:10:18 -0700

    try

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,4 @@
+#ifndef PL_H
+#define PL_H
+#define pl 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - ...
+ * Return: anything
+ */
+int main(void)
+{
+	printf("%s/n", _FILE_);
+	return (0);
+}
