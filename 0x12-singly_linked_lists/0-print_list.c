4b5304f4acd989e4bee1d8ff4988f4f706518a29
Gelilamihirke <mihirkelala@gmail.com>
2023-10-19 10:54:37 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,21 +6,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	int j = 1;
+	int j;
 
 	if (h == NULL)
 		return (0);
-	while (h->next != NULL)
+	for (j = 1; h->next != NULL; j++)
 	{
 		if (h->str == NULL)
 			printf("[%u] %s\n", h->len, "(nil)");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
-		j++;
 	}
-	printf("[%u] %s\n", h->len, "(nil)");
+	printf("[%u] %s\n", h->len, h->str);
 	return (j);
 }
