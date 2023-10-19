8a30d0254a96bcd72ad00a3bbefeb3c35929e298
Gelilamihirke <mihirkelala@gmail.com>
2023-10-19 13:05:47 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
 #include "lists.h"
+void ComeFirstbyCTC(void)__attribute__ ((constructor));
 /**
- * first - ...
+ * ComeFirst - ...
  */
-void first(void)
+void ComeFirstbyCTC(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
