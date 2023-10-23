4184a822eba1ba913e268d19afcb13538d7955ac
Gelilamihirke <mihirkelala@gmail.com>
2023-10-23 07:27:00 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,16 +8,12 @@
  *
  * Description: ...
  */
-/**
- * dog_t - ...
- */
-typedef struct dog dog_t;
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
