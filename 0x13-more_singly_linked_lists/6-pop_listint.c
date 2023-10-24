7a6da5cd5052b37b881ea2e7c49fe1df492d4884
Gelilamihirke <mihirkelala@gmail.com>
2023-10-24 08:25:20 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - ...
+ * @head: ...
+ * Return: ...
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
