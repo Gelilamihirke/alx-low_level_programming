8f43dd8876eeace8476212a7390d6be81293e53c
Gelilamihirke <mihirkelala@gmail.com>
2023-10-24 07:15:40 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - ...
+ * @h: ...
+ * Return: ...
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t c;
+
+	if (h == NULL)
+		return (0);
+	for (c = 0; h != NULL; c++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (c);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - ...
+ * @n: ...
+ * @next: ...
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t listint_len(const listint_t *h);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+#endif
