2c4bc50db01056a446cba4e1d33ef916019834cc
gelila mihirke <mihirkelala@gmail.com>
2023-12-01 09:52:01 -0800

    please work out

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * append_text_to_file - fun that app a text
+ * @filename : name of the file
+ * @text_content: the txt
+ * Return: 1 for succ, 0 for falier
+ */
+int append_text_to_file(const char *filename, const char *text_content)
+{
+	int fd;
+	ssize_t nwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	nwr = write(fd, text_content, strlen(text_content));
+	if (nwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
