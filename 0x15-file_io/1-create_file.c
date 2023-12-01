5d0a1cc28622ee044c10053865e2c69f60d60ec0
gelila mihirke <mihirkelala@gmail.com>
2023-12-01 09:34:19 -0800

    it is working

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, const char *text_content)
+{
+	int fd;
+	ssize_t nwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	nwr = write(fd, text_content, strlen(text_content));
+	if (nwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
