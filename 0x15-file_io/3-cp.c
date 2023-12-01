4138a026cc469dda5982c2a4343c9a0aa87af9eb
gelila mihirke <mihirkelala@gmail.com>
2023-12-01 12:20:39 -0800

    please work

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define BUF_SIZE 1024
+#include "main.h"
+#define BUFFER_SIZE 1024
+#define USAGE_EXIT 97
+#define READ_ERROR 98
+#define WRITE_ERROR 99
+#define CLOSE_ERROR 100
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- * Return: ...
+ * close_files - ...
+ * @src_fd: ...
+ * @dest_fd: ...
+ * Return: none
+ */
+void close_files(int src_fd, int dest_fd);
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	FILE *src, *dest;
-	char buf[BUF_SIZE];
-	size_t n;
+	int src_fd, dest_fd, num_read_bytes;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s source destination\n", argv[0]);
-		exit(1);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(USAGE_EXIT);
 	}
-	src = fopen(argv[1], "r");
-	if (src == NULL)
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
 	{
-		perror("Error opening source file");
-		exit(2);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(READ_ERROR);
 	}
-	dest = fopen(argv[2], "w");
-	if (dest == NULL)
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT
+	| O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (dest_fd == -1)
 	{
-		perror("Error opening destination file");
-		fclose(src);
-		exit(3);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_files(src_fd, dest_fd);
+		exit(WRITE_ERROR);
 	}
-	while ((n = fread(buf, 1, BUF_SIZE, src)) > 0)
+	while ((num_read_bytes = read(src_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		if (fwrite(buf, 1, n, dest) != n)
+		if (write(dest_fd, buffer, num_read_bytes) != num_read_bytes)
 		{
-			perror("Error writing to destination file");
-			exit(4);
+			dprintf(STDERR_FILENO, "Error writing to %s\n", argv[2]);
+			close_files(src_fd, dest_fd);
+			exit(WRITE_ERROR);
 		}
 	}
-	if (ferror(src))
+	close_files(src_fd, dest_fd);
+	exit(EXIT_SUCCESS);
+}
+/**
+ * close_files - ...
+ * @src_fd: ...
+ * @dest_fd: ...
+ * Return: ...
+ */
+void close_files(int src_fd, int dest_fd)
+{
+	if (close(src_fd) == -1)
 	{
-		perror("Error reading source file");
-		exit(5);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
+		exit(CLOSE_ERROR);
 	}
-	fclose(src);
-	fclose(dest);
-	return (0);
+	if (close(dest_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+		exit(CLOSE_ERROR);
+	}
 }
